9ff116f02c3c9f607db82830e2919e51e4ded33f
Sudhindra <sudhindra@Admins-Air.attlocal.net>
2023-06-24 22:09:45 -0500

    Added 1768

---
+++ 1768_Merge_Strings_Alternately.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(mergeAlternately("Hello", "Word"))
+}
+
+func mergeAlternately(str1 string, str2 string) string {
+
+	m, n := len(str1), len(str2)
+	ans := make([]byte, 0, m+n)
+	for i := 0; i < m || i < n; i++ {
+		if i < m {
+			ans = append(ans, str1[i])
+		}
+		if i < n {
+			ans = append(ans, str2[i])
+		}
+	}
+	return string(ans)
+
+}
